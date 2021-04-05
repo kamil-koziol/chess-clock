47d31ce149bb05abc0e5e70d51b4aef699ed5c55
Kamil Kozioł <koziol.kamil@protonmail.com>
2021-04-05 14:22:08 +0200

    Initial Commit

---
+++ Chess Clock/Chess_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chess_ClockApp.swift
+//  Chess Clock
+//
+//  Created by Kamil Kozioł on 05/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Chess_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chess Clock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chess Clock
+//
+//  Created by Kamil Kozioł on 05/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
